98263a50d5d2b495653e12e02949902b11055064
SarahHarrington <Sarah.Soberg@gmail.com>
2017-10-17 13:12:23 -0500

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -11,5 +11,12 @@ UPDATE "syntax_practice" SET "account_balance"='10.00' WHERE "account_balance" =
 SELECT * FROM "syntax_practice" WHERE "transactions_attempted" = '0';
 
 --5. Get the username and account balance of the 3 users with the highest balances, sort highest to lowest balance. NOTE: Research LIMIT
-SELECT * FROM "syntax_practice" ORDER BY "account_balance" DESC LIMIT 3;
+SELECT "username", "account_balance" FROM "syntax_practice" ORDER BY "account_balance" DESC LIMIT 3;
+
+--6. Get the username and account balance of the 3 users with the lowest balances, sort lowest to highest balance.
+SELECT "username", "account_balance" FROM "syntax_practice" ORDER BY "account_balance" ASC LIMIT 3;
+
+--7. Get all users with account balances that are more than $100.
+SELECT "username" FROM "syntax_practice" WHERE "account_balance" > 100;
+
 
